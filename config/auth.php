837b5ee81a8eab6998dffa6ce271ba6bc86e2ac2
Edson Gomes <edsongomex@gmail.com>
2021-08-18 09:57:26 +0100

    Configuracao do Laravel PassPort

--- config/auth.php
+++ config/auth.php
@@ -40,6 +40,10 @@ return [
             'driver' => 'session',
             'provider' => 'users',
         ],
+        'api' => [  
+            'driver' => 'passaporte' ,  
+            'provedor' => 'usuários' ,  
+        ],
     ],
 
     /*
