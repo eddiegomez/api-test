8472352f73ed57994f2495181e00d3eee0dc23cd
Edson Gomes <edsongomex@gmail.com>
2021-08-20 10:27:24 +0100

    versao preliminar

--- app/Http/Controllers/API/EntidadeController.php
+++ app/Http/Controllers/API/EntidadeController.php
@@ -8,7 +8,7 @@ use App\Models\Entidade;
 use Validator;
 use DB;
 use App\Http\Resources\Entidade as EntidadeResource;
-   
+
 class EntidadeController extends BaseController
 {
     /**
@@ -20,30 +20,15 @@ class EntidadeController extends BaseController
     {
         try {
             DB::connection()->getPdo();
-            $tabelas = DB::select('select * FROM igss.api_inss OFFSET 20 ROWS FETCH NEXT 10 ROWS ONLY');
-            dd($tabelas);
+
+            $entidades = DB::select('select * FROM igss.api_inss FETCH NEXT 100 ROWS ONLY');
+            $entidades =  response()->json($entidades, 200);
+            //$entidades = $this->sendResponse(EntidadeResource::collection($entidades), 'Entidades retrieved successfully.');
+            return $entidades;
+
         } catch (\Exception $e) {
             die("Could not connect to the database.  Please check your configuration. error:" . $e );
         }
-        /*$entidade = Entidade::all();
-        return $this->sendResponse(EntidadeResource::collection($entidade), 'Entidades retrieved successfully.');*/
     }
 
-    public static function convert_from_latin1_to_utf8_recursively($dat)
-    {
-       if (is_string($dat)) {
-          return utf8_encode($dat);
-       } elseif (is_array($dat)) {
-          $ret = [];
-          foreach ($dat as $i => $d) $ret[ $i ] = self::convert_from_latin1_to_utf8_recursively($d);
- 
-          return $ret;
-       } elseif (is_object($dat)) {
-          foreach ($dat as $i => $d) $dat->$i = self::convert_from_latin1_to_utf8_recursively($d);
- 
-          return $dat;
-       } else {
-          return $dat;
-       }
-    }
 }
